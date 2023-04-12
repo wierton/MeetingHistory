046f7d5cda37c72f5fd3d722f06b844c8705bfd5
wierton <ouxianfei@smail.nju.edu.cn>
2023-04-12 10:42:48 +0800

    add style for link

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ def delete_recommended_paper(paper_id):
     return redirect(url_for('recommended_paper_submission'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
